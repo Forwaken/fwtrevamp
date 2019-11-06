831a27bf3d370c8eb3f1281dbed84a5126588a9a
asamid <cbower7534@gmail.com>
2019-11-06 15:12:21 -0500

    Update bX-ui.js

--- j/librerias/bX-ui.js
+++ j/librerias/bX-ui.js
@@ -21,8 +21,6 @@ var ui = function () {
 	_iv1 = new Image(); _iv1.src = "g/loader.png"; _iv1.onload = preLoader;
 //
 
-	$('.footcoin#foot #right').html($.i18n._('coincount') + fwt.coincount.toFixed(2));
-
 //	<Init> prepares user interface
 	var savedGameImage = null;
 	var activeGame = false;
